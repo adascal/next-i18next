a6866a230f5d16c113c48c6a23c912605f4bcb26
Alexandr Dascal <a.dascal@semrush.com>
2019-07-26 00:33:24 +0300

    Show dynamic routing issue example

---
+++ examples/simple/pages/post/[id].js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { withTranslation, Link } from '../../i18n';
+
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+const Post = ({ t }) => (
+  <React.Fragment>
+    <main>
+      <Header title={t('h1')} />
+      <Link href="/">
+        <button type="button">{t('back-to-home')}</button>
+      </Link>
+    </main>
+    <Footer />
+  </React.Fragment>
+);
+Post.getInitialProps = async () => ({
+  namespacesRequired: ['second-page', 'footer'],
+});
+
+Post.propTypes = {
+  t: PropTypes.func.isRequired,
+};
+
+export default withTranslation('second-page')(Post);
